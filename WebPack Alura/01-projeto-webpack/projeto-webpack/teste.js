eee91413df2f27e55f8487a64435dfcf93b59992
Igor Carvalho <igor.carvalhox8@gmail.com>
2021-06-04 09:13:11 -0300

    LocalStorage  Teste

--- WebPack Alura/01-projeto-webpack/projeto-webpack/teste.js
+++ WebPack Alura/01-projeto-webpack/projeto-webpack/teste.js
@@ -1,7 +1,17 @@
+var nome = window.document.querySelector('input#nome')
+var senha = window.document.querySelector('input#senha')
+
+function cadastra(){
+    localStorage.setItem('nome',nome.value)
+    localStorage.setItem('senha', senha.value)
+}
+
 
 function testar(){
-a = 1
-if(a == 1){
+if(localStorage.getItem('nome') == nome.value && localStorage.getItem('senha') == senha.value){
     window.location.href = "https://www.google.com.br/"
+}
+else{
+    alert('Login ou senha incorretos')
 }
 }
