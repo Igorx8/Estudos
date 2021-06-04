5e0b5f418d8f51bbb7acf8fbe07397cb3a2f3b1e
Igor Carvalho <igor.carvalhox8@gmail.com>
2021-06-04 16:18:51 -0300

    LocalStorage Completo

--- WebPack Alura/01-projeto-webpack/projeto-webpack/teste.js
+++ WebPack Alura/01-projeto-webpack/projeto-webpack/teste.js
@@ -1,23 +1,41 @@
 var nome = window.document.querySelector('input#nome')
 var senha = window.document.querySelector('input#senha')
 
-function cadastra(){
-    localStorage.setItem('nome',nome.value)
-    localStorage.setItem('senha', senha.value)
+const dados = []
+
+function cadastra(){ 
+
+    dados.push(
+        { 
+            name: nome.value,
+            pwd: senha.value
+        }
+    )
+
+    localStorage.setItem('Usuario', JSON.stringify(dados))
+    alert('Cadastrou')
 }
 
 
 function testar(){
-if(localStorage.getItem('nome') == nome.value && localStorage.getItem('senha') == senha.value){
-    window.location.href = "https://www.google.com.br/"
-}
-else{
-    alert('Login ou senha incorretos')
-}
+    let tabela = JSON.parse(localStorage.Usuario)
+    let logado = 0
+    for(i = 0; i < tabela.length; i++){
+        if(tabela[i].name == nome.value && tabela[i].pwd == senha.value){
+            logado += 1
+        }
+    }
+    if(logado == 1){ 
+        alert('Logado')
+        window.location.href="https://github.com/Igorx8"
+    }
+    else{
+        alert('Dados inválidos')
+    }
 }
 
 function deleta(){
-    if(localStorage.getItem('nome',nome.value)){
+    if(localStorage.getItem('nome') == nome.value){
         localStorage.removeItem('nome',nome.value)
         localStorage.removeItem('senha',senha.value)
     }
