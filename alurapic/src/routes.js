579cb4bf6c63b599a167ba713ae06dd066e7d8a2
Igor Carvalho <igor.carvalhox8@gmail.com>
2021-06-08 17:06:53 -0300

    .

--- alurapic/src/routes.js
+++ alurapic/src/routes.js
@@ -1,7 +1,9 @@
 import Cadastro from './components/cadastro/Cadastro.vue';
 import Home from './components/home/Home.vue';
+import Grafico from './components/graficos.vue'
 
 export const routes = [
     { path: '', component: Home, titulo: 'Home'},
-    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro'}
+    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' },
+    { path: '/graficos', component: Grafico, titulo: 'Graficos'}
 ];
