912f08f2a8d1f8e0f7b48f9ab0176597ef93286f
Igor <igor.carvalhox8@gmail.com>
2021-10-08 16:27:22 -0300

    modulo regex em dev

--- NodeJs/Primeira biblioteca em NodeJS/index.js
+++ NodeJs/Primeira biblioteca em NodeJS/index.js
@@ -1,6 +1,14 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
+
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
+    const linksExtraidos = texto.match(regex);
+    console.log(linksExtraidos)
+}
+
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'O arquivo ou diretório não foram encontrados, erro(' + erro.code + ')'))
 }
@@ -18,7 +26,8 @@ async function pegaArquivo(caminhoDoArquivo){
     
 }
 
-pegaArquivo('./arquivos/texto1.md')
+extraiLinks(texto)
+// pegaArquivo('./arquivos/texto1.md')
 
 // .then()  e .catch()
 // function pegaArquivo(caminhoDoArquivo){
