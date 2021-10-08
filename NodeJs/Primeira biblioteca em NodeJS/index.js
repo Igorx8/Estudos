6d664c825507b4c9ed0e1d78378d305bc675c25b
Igor <igor.carvalhox8@gmail.com>
2021-10-08 10:15:53 -0300

    Módulo 2 concluído

--- NodeJs/Primeira biblioteca em NodeJS/index.js
+++ NodeJs/Primeira biblioteca em NodeJS/index.js
@@ -1,18 +1,66 @@
 const chalk = require('chalk')
-const log = (a) => console.log(a)
+const fs = require('fs')
 
-log('vamos começar!!')
-
-const paragrafo = 'Texto retornado por uma função'
-
-function texto(string){
-    return string
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'O arquivo ou diretório não foram encontrados, erro(' + erro.code + ')'))
 }
 
-log(chalk.blue(texto(paragrafo)))
+//async | await
+async function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8'
+    try{
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto))
+    }
+    catch(erro){
+        trataErro(erro)
+    }
+    
+}
 
-log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+pegaArquivo('./arquivos/texto1.md')
+
+// .then()  e .catch()
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8'
+//     fs.promises.readFile(caminhoDoArquivo, encoding)
+//     .then((data) => console.log(chalk.yellow(data)))
+//     .catch((erro) => trataErro(erro))
+// }
+
+// pegaArquivo('./arquivos/texto1.md')
+
+
+
+// funcao normal
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoDoArquivo, encoding, (err, data) => {
+//         if(err){
+//             trataErro(err)
+//         }
+//         else{
+//             console.log(chalk.green(data))
+//         }
+        
+//     })
+    
+// }
+
+// pegaArquivo('./arquivos/texto1.md')
+
+
+// const log = (a) => console.log(a)
+// const paragrafo = 'Texto retornado por uma função'
+
+// function texto(string){
+//     return string
+// }
+
+// log(chalk.blue(texto(paragrafo)))
+
+// log(`
+// CPU: ${chalk.red('90%')}
+// RAM: ${chalk.green('40%')}
+// DISK: ${chalk.yellow('70%')}
+// `);
