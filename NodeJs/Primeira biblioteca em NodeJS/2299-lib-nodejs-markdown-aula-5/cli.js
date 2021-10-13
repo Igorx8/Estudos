64159d163d56ea25edf104e047a4f3bca24f566e
Igor <igor.carvalhox8@gmail.com>
2021-10-13 14:58:13 -0300

    Módulo 5

--- NodeJs/Primeira biblioteca em NodeJS/2299-lib-nodejs-markdown-aula-5/cli.js
+++ NodeJs/Primeira biblioteca em NodeJS/2299-lib-nodejs-markdown-aula-5/cli.js
@@ -1,18 +1,16 @@
 const chalk = require('chalk');
 const pegaArquivo = require('./index');
-const validaUrls = require('./http-validacao');
+const validaURLs = require('./http-validacao');
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
   const resultado = await pegaArquivo(caminhoDeArquivo[2]);
-  if(caminhoDeArquivo[3] === 'validar'){
-      console.log(chalk.yellow('Links validados'), await validaUrls(resultado))
-  }
-  else{
+  if (caminho[3] === 'validar') {
+    console.log(chalk.yellow('links validados'), await validaURLs(resultado));
+  } else {
     console.log(chalk.yellow('lista de links'), resultado);
   }
-  
 }
 
 processaTexto(caminho);
--- NodeJs/Primeira biblioteca em NodeJS/http-validacao.js
+++ NodeJs/Primeira biblioteca em NodeJS/http-validacao.js
@@ -1,25 +1,44 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
-async function checaStatus(arrayUrls){
-    const arrayStatus = await Promise.all(arrayUrls.map(async url =>{
-        const res = await fetch(url)
-        return res
-    }))
-    return arrayStatus
+function manejaErros(erro) {
+  throw new Error(erro.message);
 }
 
-function geraArrayDeUrls(arrayLinks){
-    const links = []
-    for(elem of arrayLinks){
-        links.push(Object.values(elem))
-    }
-    return links
+async function checaStatus(arrayUrls) {
+  try {
+    const arrayStatus = await Promise.all(
+      arrayUrls.map(async (url) => {
+        const res = await fetch(url);
+        return `${res.status} - ${res.statusText}`;
+      })
+    );
+    return arrayStatus;
+  } catch (erro) {
+    manejaErros(erro);
+  }
 }
 
-async function validaUrls(arrayLinks){
-    const links = geraArrayDeUrls(arrayLinks)
-    const statusLinks = await checaStatus(links)
-    return statusLinks
+function geraArrayDeUrls(arrayLinks) {
+  const links = [];
+  for (elem of arrayLinks) {
+    links.push(Object.values(elem));
+  }
+  return links;
 }
 
-module.exports = validaUrls
+async function validaUrls(arrayLinks) {
+  try {
+    const links = geraArrayDeUrls(arrayLinks);
+    const statusLinks = await checaStatus(links);
+
+    const resultados = arrayLinks.map((objeto, indice) => ({
+      ...objeto,
+      status: statusLinks[indice],
+    }));
+    return resultados;
+  } catch (erro) {
+    manejaErros(erro);
+  }
+}
+
+module.exports = validaUrls;
