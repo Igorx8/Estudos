044c6317705dfa4eebc621fb9ba4d1aa439ce265
Igor <igor.carvalhox8@gmail.com>
2021-10-13 09:56:33 -0300

    update

--- NodeJs/Primeira biblioteca em NodeJS/index.js
+++ NodeJs/Primeira biblioteca em NodeJS/index.js
@@ -1,13 +1,15 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
-const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
-
-function extraiLinks(texto){
-    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
-    const linksExtraidos = texto.match(regex);
-    console.log(linksExtraidos)
-}
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while((temp = regex.exec(texto)) !== null) {
+      arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+    return arrayResultados.length === 0 ? 'não há links' : arrayResultados;
+  }
 
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'O arquivo ou diretório não foram encontrados, erro(' + erro.code + ')'))
@@ -18,7 +20,7 @@ async function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8'
     try{
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto))
+        return extraiLinks(texto)
     }
     catch(erro){
         trataErro(erro)
@@ -26,7 +28,7 @@ async function pegaArquivo(caminhoDoArquivo){
     
 }
 
-extraiLinks(texto)
+module.exports = pegaArquivo;
 // pegaArquivo('./arquivos/texto1.md')
 
 // .then()  e .catch()
