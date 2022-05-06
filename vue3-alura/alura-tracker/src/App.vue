0c2afc7fe637955aa48ce6cf8ff76706f09790bf
igorx8 <igor.carvalhox8@gmail.com>
2022-05-06 18:47:08 -0300

    update

---
+++ vue3-alura/alura-tracker/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <Formulario />
+      <div class="lista">
+        <Tarefa />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
+import Tarefa from './Tarefa.vue'
+
+export default defineComponent({
+    name: "App",
+    components: { BarraLateral, Formulario, Tarefa }
+});
+</script>
+
+<style>
+</style>
