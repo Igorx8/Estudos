b05dae6950da496af82627af296c8c5eea3eee29
igorx8 <igor.carvalhox8@gmail.com>
2022-06-01 17:27:06 -0300

    update ex votation

---
+++ TypeScript/tscompleto/src/A0051-exercicio-generics/votacao.ts
@@ -0,0 +1,47 @@
+type Linguagens = Record<string, number>;
+
+type objModel = { [k: string | number]: number };
+
+type OpcoesVotos = <O, K extends keyof O> (Obj: O, chave: K) => O[K];
+
+const votacao1: Linguagens = {
+  Python: 0,
+  Javascript: 0,
+  Typescript: 0
+}
+
+class Votacao {
+  private _index: number = 0;
+  private _opcoes: objModel = {};
+  constructor(
+    private opcoes: objModel
+  ) {
+    this._opcoes = { ...opcoes };
+    if (opcoes) {
+      this._index++;
+    }
+
+  }
+
+  private emptyOptions(): boolean {
+    return this._index === 0;
+  }
+
+  showVotes(): objModel | string {
+    if (!this.emptyOptions()) return this._opcoes;
+    return 'Votação vazia, adicione opções!'
+  }
+
+  votar(chave: string | number): objModel | string {
+    if (chave) {
+      this._opcoes[chave]++;
+      return this.showVotes();
+    }
+    return `Digite a opção de voto desejada!`
+  }
+}
+
+const votacao = new Votacao(votacao1);
+console.log(votacao.votar('Javascript'));
+console.log(votacao.votar('Javascript'));
+console.log(votacao.votar('Python'));
