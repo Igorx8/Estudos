9c5cad3a153536c8b9f676f084722b97f4e56f9e
igorx8 <igor.carvalhox8@gmail.com>
2022-07-14 14:15:13 -0300

    crud basico dentro da propria pagina

--- React/lista-tarefas/.eslintrc.js
+++ React/lista-tarefas/.eslintrc.js
@@ -29,6 +29,6 @@ module.exports = {
     },
   },
   rules: {
-    'react/react-in-jsx-scope': 'off',
+    // 'react/react-in-jsx-scope': 'off',
   },
 };
--- React/lista-tarefas/src/components/Main.js
+++ React/lista-tarefas/src/components/Main.js
@@ -1,27 +1,99 @@
 import React, { Component } from 'react';
 
+//Form
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [],
+    index: -1,
   };
 
   handleChange = (e) => this.setState({ novaTarefa: e.target.value });
 
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  };
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim(); //o trim remove os espaços enviados na string
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
+  };
+
   render() {
+    const { novaTarefa, tarefas } = this.state;
+
     return (
       <div className="main">
         <h1> Lista de tarefas </h1>
 
-        <form action="#">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input
             onChange={this.handleChange}
             type="text"
             placeholder="Adicionar tarefa"
+            value={novaTarefa}
           />
-          <button type="submit">Adicionar</button>
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa, index) => (
+            <li key={tarefa}>
+              {tarefa}
+              <span>
+                <FaEdit
+                  onClick={(e) => this.handleEdit(e, index)}
+                  className="edit"
+                />
+                <FaWindowClose
+                  onClick={(e) => this.handleDelete(e, index)}
+                  className="delete"
+                />
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
