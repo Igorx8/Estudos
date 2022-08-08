212350995ea01acd943eed9fc7eb49939d442b5f
igorx8 <igor.carvalhox8@gmail.com>
2022-08-08 17:09:57 -0300

    node ts

---
+++ NodeJs/nodets-rocketseat/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import routes from './routes';
+
+class App {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+
+    this.middleWares();
+    this.database();
+    this.routes();
+  }
+
+  private middleWares(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
+
+  private database(): void {
+    mongoose.connect('mongodb://root:example@127.0.0.1:27017/api?authSource=admin', {
+      // CONNECTIONSTRING=mongodb+srv://ec2-user:joao1314@cluster0.0loudcn.mongodb.net/AGENDA?retryWrites=true&w=majority
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(() => console.log('logou')).catch((err) => console.log(err));
+  }
+
+  private routes(): void {
+    this.express.use(routes);
+  }
+}
+
+export default new App().express
